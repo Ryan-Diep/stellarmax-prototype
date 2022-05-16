2fcbd413b06fd1e5e3f09722d0cfb6032c6fbdcf
Ryan Diep <ryandiep5@gmail.com>
2022-05-16 13:12:49 -0400

    first commit

---
+++ src/main/java/com/stellarmaxprototype/StellarmaxprototypeApplication.java
@@ -0,0 +1,13 @@
+package com.stellarmaxprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarmaxprototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellarmaxprototypeApplication.class, args);
+	}
+
+}
