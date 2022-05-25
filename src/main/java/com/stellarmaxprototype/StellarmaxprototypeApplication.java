665e5d307a8b3939c2aa5234134ab114bcaa4a7c
Ryan Diep <ryandiep5@gmail.com>
2022-05-25 14:07:00 -0400

    following java techie guide and using azure sql

--- src/main/java/com/stellarmaxprototype/StellarmaxprototypeApplication.java
+++ src/main/java/com/stellarmaxprototype/StellarmaxprototypeApplication.java
@@ -1,13 +1,33 @@
 package com.stellarmaxprototype;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StellarmaxprototypeApplication {
 
+	@Autowired
+	private EmployeeRepository repository;
+	
+	public Employee addEmployee(@RequestBody Employee employee) {
+		return repository.save(employee);
+	}
+	
+	@PostMapping("/employee")
 	public static void main(String[] args) {
 		SpringApplication.run(StellarmaxprototypeApplication.class, args);
 	}
-
+	
+	@GetMapping("/employees")
+	public List<Employee> getEmployees(){
+		return repository.findAll();
+	}
 }
