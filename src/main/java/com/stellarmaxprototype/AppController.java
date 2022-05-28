b86849530a9a6bd5c55e4af9d3978fa71c17f7b8
Ryan Diep <ryandiep5@gmail.com>
2022-05-28 13:01:12 -0400

    adding thymeleaf

--- src/main/java/com/stellarmaxprototype/AppController.java
+++ src/main/java/com/stellarmaxprototype/AppController.java
@@ -3,19 +3,50 @@ package com.stellarmaxprototype;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
+@Controller
 public class AppController {
-
 	@Autowired
-	private EmployeeRepository repository;
+	private UserRepository repo;
 	
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String showSignupForm(Model model) {
+			model.addAttribute("user", new User());
+			
+			return "signup_form";
+	}
+	
+	@PostMapping("/process_register")
+	public String processRegistration(User user) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	    String encodedPassword = passwordEncoder.encode(user.getPassword());
+	    user.setPassword(encodedPassword);
+		repo.save(user);
+		
+		return "register_success";
+	}
+	
+	@GetMapping("/list_users")
+	public String viewUserList(Model model) {
+		List<User> listUsers = repo.findAll();
+	    model.addAttribute("listUsers", listUsers);
+	    
+		return "users";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 	
 //	public Employee addEmployee(@RequestBody Employee employee) {
